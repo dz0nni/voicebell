701bea1d31cf5c49e3cb0545349be3434fdeb6f6
dzOnni <dz0nni@user.local>
2025-11-21 22:59:59 +0200

    chore: bump version to 0.1.42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.voicebell.clock"
         minSdk = 29
         targetSdk = 35
-        versionCode = 42
-        versionName = "0.1.41"
+        versionCode = 43
+        versionName = "0.1.42"
 
         testInstrumentationRunner = "com.voicebell.clock.HiltTestRunner"
         vectorDrawables {
